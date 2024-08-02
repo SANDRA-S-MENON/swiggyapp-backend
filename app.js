3362438a20f8547b3570ea105af04061c4cfeb4a
SANDRA-S-MENON <sandramenon2002@gmail.com>
2024-08-02 22:09:18 +0530

    signup created

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const mongoose=require("mongoose")
 const cors=require("cors")
 const express=require("express")
 const bcrypt=require("bcrypt")
-
+const userModel = require("./models/user")
 const app=express()
 app.use(cors())
 app.use(express.json())
@@ -11,11 +11,23 @@ mongoose.connect("mongodb+srv://sandras02:sandrasmenon@cluster0.3g103sn.mongodb.
 
 
 
-app.get("/",(req,res)=>{
-        res.json("hello")
+
+
+
+    app.post("/usersignup",(req,res)=>{
+        let input=req.body
+        let hashedpassword=bcrypt.hashSync(input.password,10)
+        //console.log(hashedpassword)
+        input.password=hashedpassword
+        console.log(input) 
+        let result= new userModel(input)
+        result.save()
+        res.json({"status":"success"})
+        
     })
-    
+
 
 app.listen(4040,()=>{
+
     console.log("server started")
 })
